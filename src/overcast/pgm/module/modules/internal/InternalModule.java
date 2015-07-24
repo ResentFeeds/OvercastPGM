a074e933aafc534fe03a0fdf78eb6d61a6f31077
SkylerTyler1337 <tylernichols409@gmail.com>
2015-07-24 02:55:00 -0500

    forgot ModuleInfo Annotation :)

--- src/overcast/pgm/module/modules/internal/InternalModule.java
+++ src/overcast/pgm/module/modules/internal/InternalModule.java
@@ -4,12 +4,17 @@ import org.bukkit.event.HandlerList;
 
 import overcast.pgm.builder.Builder;
 import overcast.pgm.module.Module;
+import overcast.pgm.module.ModuleInfo;
 
+@ModuleInfo(name = "internal", desc = "Prevent compass teleports above Y 255")
 public class InternalModule extends Module{
 
+	// Not done with this yet 
+	
 	private boolean internal;
 
-	public InternalModule(){}
+	public InternalModule(){ 
+	}
 	
 	public InternalModule(boolean internal){
 		this.internal = internal;
